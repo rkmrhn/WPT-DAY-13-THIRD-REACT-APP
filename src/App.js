6bb2b434d1503f212475f67766e5f7e970b3e95b
Rohan Maurya <rohanmaurya1695@gmail.com>
2023-06-14 15:48:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 function App() {
-  let count = 1;
-  let MyCounterFun=()=>{
-    alert("how r you");
-  }
-
   return (
     <>
-      <div className="ms-5">
-        <h1>Counter app</h1>
-        <h1>1</h1>
-        <input type="button" value="like me" onClick={MyCounterFun}/>
-      </div>
+      <h1>Counter app</h1>
+      <CounterApp/>
     </>
   );
 }
+function CounterApp() {
+  let count = 1;
 
+  let MyCounterApp = () => {
+    count = count + 1;
+    console.log(count);
+  };
+  return (
+    <>
+      <h1>{count}</h1>
+      <input type="button" value="like me" onClick={MyCounterApp} />
+    </>
+  );
+}
 export default App;
